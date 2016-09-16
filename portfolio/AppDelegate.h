eabe9db7b660f813b6f522274baf7ababb961399
venkat reddy <venkat.ios1992@gmail.com>
2016-09-16 11:49:49 -0500

    portfolio App

---
+++ portfolio/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  portfolio
+//
+//  Created by Venkat on 9/16/16.
+//  Copyright © 2016 iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ portfolio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  portfolio
+//
+//  Created by Venkat on 9/16/16.
+//  Copyright © 2016 iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
